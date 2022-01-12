16adc53976cf930937c509f0d414cd6f76bd96c7
A113942 <Daniel_E_Sarinana@progressive.com>
2022-01-12 17:16:11 -0500

    Cloned string calc from class

---
+++ StringCalculatorUnitTests/IWebService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorUnitTests
+{
+    public interface IWebService
+    {
+        void Notify(string message);
+    }
+}
